c281937f1b7f2f71fd9d494ff7094dc8ae181b77
SunHeng <svbv5453@163.com>
2017-02-23 16:56:58 +0800

    3、完成ssm框架搭建；并通过mybatis-generator逆向工程生成po、mapper接口和mapper.xml；添加了mybatis的PageHelper分页插件。

---
+++ src/test/java/com/sh/ssm/taotao/controller/MainControllerTest.java
@@ -0,0 +1,46 @@
+package com.sh.ssm.taotao.controller;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.sh.ssm.taotao.mapper.TbItemMapper;
+import com.sh.ssm.taotao.po.TbItem;
+import com.sh.ssm.taotao.po.TbItemExample;
+import junit.framework.TestCase;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/2/23.
+ */
+public class MainControllerTest extends TestCase {
+
+    private TbItemMapper tbItemMapper;
+    public void setUp() throws Exception {
+        super.setUp();
+        String path = "classpath:applicationContext.xml";
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext(path);
+        tbItemMapper = (TbItemMapper) applicationContext.getBean(TbItemMapper.class);
+
+
+    }
+
+    public void testShowIndex() throws Exception {
+
+    }
+
+    public void testPageHelper() throws Exception {
+        PageHelper.startPage(1, 30);
+        TbItemExample example = new TbItemExample();
+        List<TbItem> list = tbItemMapper.selectByExample(example);
+        PageInfo<TbItem> pageInfo = new PageInfo<TbItem>(list);
+        int pageNum = pageInfo.getPageNum();
+        long total = pageInfo.getTotal();
+        int pages = pageInfo.getPages();
+        int size = pageInfo.getSize();
+        System.out.println("页数" + pageNum + "总数" + total + "pages" + pages + "size" + size);
+
+    }
+
+}
